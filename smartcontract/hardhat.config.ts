acb00635c7d988a475f458320928aee1f1d0afce
Richiey1 <ojodamilarey@gmail.com>
2026-01-03 14:20:20 +0100

    build: add OpenZeppelin upgrades dependencies
    - Installed @openzeppelin/contracts-upgradeable v5.4.0
    - Installed @openzeppelin/hardhat-upgrades for proxy deployment
    - Updated hardhat.config.ts to import upgrades plugin
    - Added 716 packages for UUPS upgradeability support
    
    Issue #14

--- smartcontract/hardhat.config.ts
+++ smartcontract/hardhat.config.ts
@@ -1,6 +1,7 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-chai-matchers";
+import "@openzeppelin/hardhat-upgrades";
 import "hardhat-gas-reporter";
 
 // If not set, it uses ours Alchemy's default API key.
